85ae64102105fcc1861ed04f5cc742841b61c4f3
Aswin Koliyot <akoliyot@gmail.com>
2018-12-26 19:25:42 +0100

    Add new quotes and update styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,71 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-
 import Layout from '../components/layout'
-
 import '../styles/app.scss';
 import * as styles from './index.module.scss';
 
-class IndexPage extends React.Component {
-  render() {
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
+let classNames = require('classnames');
 
-    let allQuotes, randomIndex, selectedQuote;
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedQuote: this.getRandomQuote()
+    }
+  }
+
+  handleKeyPress = (e) => {
+    console.log(e);
+
+    switch (e.keyCode) {
+      case 78: 
+        console.log('Show next quote');
+        this.setState({
+          selectedQuote: this.getRandomQuote()
+        });
+        console.log(this.state.selectedQuote);
+        break;
+
+      case 80: 
+        console.log('Show previous quote');
+        break;
+
+      default: 
+        console.log('Unknown key pressed');
+    }
+  }
+
+  getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  getRandomQuote = () => {
+    let allQuotes, randomIndex;
 
     allQuotes = this.props.data.allQuotesJson.edges;
-    randomIndex = getRandomInt(allQuotes.length);
-    selectedQuote = allQuotes[randomIndex].node;
+    randomIndex = this.getRandomInt(allQuotes.length);
+    return allQuotes[randomIndex].node;
+  }
 
+  componentDidMount = () => {
+    console.log('componentDidMount');
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount = () => {
+    console.log('componentWillUnmount');
+    document.removeEventListener('keypress', this.handleKeyPress);
+  }
+  
+  render() {
     return (
       <Layout>
-        <div className={styles.contentBox}>
+        <div className={classNames(styles.contentBox, 'hidden')}>
           <article>
-            <h1>{selectedQuote.text}</h1>
+            <h1>{this.state.selectedQuote.text}</h1>
             <footer>
-              {selectedQuote.author ? <p className={styles.author}>{selectedQuote.author}</p> : ''}
-              {selectedQuote.source ? <p className={styles.source}>{selectedQuote.source}</p> : ''}
+              {this.state.selectedQuote.author ? <p className={styles.author}>{this.state.selectedQuote.author}</p> : ''}
+              {this.state.selectedQuote.source ? <p className={styles.source}>{this.state.selectedQuote.source}</p> : ''}
             </footer>
           </article>
         </div>
@@ -33,36 +73,6 @@ class IndexPage extends React.Component {
     );
   }
 }
-
-// const IndexPage = ({data}) => {
-//   let allQuotes, randomIndex, selectedQuote;
-
-//   allQuotes = data.allQuotesJson.edges;
-//   console.log(allQuotes);
-  
-//   randomIndex = getRandomInt(allQuotes.length);
-//   selectedQuote = allQuotes[randomIndex].node;
-
-//   function getRandomInt(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-//   }
-
-//   return (
-//     <Layout>
-//       <div className={styles.contentBox}>
-//         <article>
-//           <h1>{selectedQuote.text}</h1>
-//           <footer>
-//             <p className={styles.author}>{selectedQuote.author}</p>
-//             <p className={styles.source}>{selectedQuote.source}</p>
-//           </footer>
-//         </article>
-        
-
-//       </div>
-//     </Layout>
-//   );
-// }
 
 export default IndexPage
 
